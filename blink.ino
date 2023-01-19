d257f8c542ff560a67e01d84925d7e3b33501d62
Eldar Venes <eldarvenes@gmail.com>
2023-01-19 14:12:28 +0100

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -11,5 +11,5 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(31);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(22);                       // wait for a second
+  delay(2w2);                       // wait for a second
 }
